c0a638376664a94ae7be594834175644da49f347
yenu <35312312+yenuGH@users.noreply.github.com>
2024-03-13 06:08:48 -0700

    finish login page

--- frontend/src/pages/login_page.tsx
+++ frontend/src/pages/login_page.tsx
@@ -3,7 +3,18 @@ import { Center, Box, Flex } from "@mantine/core"
 export function LoginPage() {
     return (
         <Center style={{ height: '100vh' }}>
-            Login Page
+            <Box style={{ width: 400 }}>
+                <Flex direction="column" gap="md">
+                    <h1 style={{ textAlign: 'center' }}>Login</h1>
+                    <input type="text" placeholder="Username" />
+                    <input type="password" placeholder="Password" />
+                    <button style={{ cursor: 'pointer' }}>Sign In</button>
+
+                    <hr />
+                    
+                    <button style={{ cursor: 'pointer' }}>Don't have an account? Sign Up</button>
+                </Flex>
+            </Box>
         </Center>
     );
 }
