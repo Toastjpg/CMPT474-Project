5a445fdab2f499a0db22cc300c83372c91544355
root <root@Galaxy.>
2024-03-13 20:16:23 -0700

    Added post router?

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
+
 
 app.get('/', (req, res) => {
   res.send('Node.js API Gateway is up and running! Easy right???');
@@ -13,3 +15,21 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`API Gateway is running on port ${PORT}`);
 });
+
+app.post('/create-post', (req, res) => {
+
+});
+
+app.get('/get-post', (req, res) => {
+
+});
+
+app.get('/update-post', (req, res) => {
+
+});
+
+app.get('/delete-post', (req, res) => {
+
+});
+
+const postRouter = require("../backend/post-service/routes/posts.controller.js")
