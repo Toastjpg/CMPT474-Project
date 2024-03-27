b3c8989739f031b181c786b4f5ea18d593d72961
jerrydngzh <jerrydeng67@gmail.com>
2024-03-27 16:40:13 -0700

    setup basics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { initializeApp } from 'firebase/app'
+
+// NOTE: This is the Firebase configuration for the project. It is not sensitive information.
+const firebaseConfig = {
+  apiKey: "AIzaSyBm5EHvIEdAUPZTg4wzbjIVbczs_6_9x8Q",
+  authDomain: "winter-berm-413619.firebaseapp.com",
+  projectId: "winter-berm-413619",
+  storageBucket: "winter-berm-413619.appspot.com",
+  messagingSenderId: "430630989801",
+  appId: "1:430630989801:web:deae0a0628009a98452578",
+  measurementId: "G-1C4G026N3X"
+};
+
+const app = initializeApp(firebaseConfig);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
--- frontend/src/pages/login_page.tsx
+++ frontend/src/pages/login_page.tsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 
 import { signinUser } from "../controllers/account.controller";
 
+import { useAuth0 } from "@auth0/auth0-react";
+
 export function SigninPage() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
@@ -14,8 +16,8 @@ export function SigninPage() {
         setButtonIdle(true)
         const response = await signinUser(username, password) // signinUser not implemented yet!!!
         const data = await response.json()
-        if(response.ok) {
-            navigate("/homepage" )
+        if (response.ok) {
+            navigate("/homepage")
             return
         }
         setButtonIdle(false)
@@ -35,9 +37,18 @@ export function SigninPage() {
                 value={password}
                 onChange={(event) => setPassword(event.currentTarget.value)}
             />
-            
+
             <Button color="gray" disabled={buttonIdle} onClick={signin} className={buttonIdle ? "spin" : ''}>Sign in</Button>
             </>
+        )
+    }
+
+    // NOTE: testing
+    const auth0Signin = () => {
+        const { loginWithRedirect } = useAuth0();
+
+        return (
+            <Button color="gray" onClick={() => loginWithRedirect()}>Sign in with Auth0</Button>
         )
     }
 
@@ -46,6 +57,7 @@ export function SigninPage() {
         <p className="designHeading">Sign in</p>
         <Title order={1} >SFU Collaborative Learning Platform</Title>
         {signinForm()}
+        {auth0Signin()}
         </>
     )
 }
