122ef4d368af007f53b968d3fb5406abf60ea5a9
jerrydngzh <jerrydeng67@gmail.com>
2024-03-30 00:44:03 -0700

    add comment

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -3,6 +3,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/home_page';
 import { RegisterPage } from './pages/register_page';
 
+// FIXME might need to refactor the way routers are defined here to allow for auth route guarding
+
 const router = createBrowserRouter([
     {
         path: '/homepage',
