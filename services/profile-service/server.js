beecd0836d6fb48d81dc92acc3501ca3e68137df
Johnny <52947117+Toastjpg@users.noreply.github.com>
2024-03-31 16:42:15 -0700

    stub profile service endpoints

--- services/profile-service/server.js
+++ services/profile-service/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const mailService = require("nodemailer");
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -20,80 +19,39 @@ app.use(express.json());
 // Get profile information
 app.get("/api/profile/:id", async (req, res) => {
     const id = req.params.id
-    res.status(200).json({ message: `Getting profile data for user with id: ${id}` })
+
+    if (id === undefined || id === null) {
+        return res.status(400).json({ error: "ERROR: Invalid ID" })
+    }
+
+    try {
+        // TODO Make db query here
+        // https://firebase.google.com/docs/firestore/query-data/get-data#get_a_document
+
+        return res.status(200).json({ message: `Getting profile data for user with id: ${id}` })
+    }
+    catch (e) {
+        console.error(e)
+        return res.status(500).json({ error: "ERROR: DB query to get failed." })
+    }
 })
 
 // Add or edit profile data
 app.put("/api/profile/", async (req, res) => {
     const id = req.body.id
-
-    // The profile content to store
-    // Probably just an object with some strings
-    // Maybe just one string called bio
-    // Maybe we wont even use this feature..
     const content = req.body.content
 
-    res.status(200).json({ message: `Creating/Updating profile data for user with id: ${id}` })
+    try {
+        // TODO make DB query here
+        //https://firebase.google.com/docs/firestore/manage-data/add-data#set_a_document
+
+        res.status(200).json({ message: `Creating/Updating profile data for user with id: ${id}` })
+    }
+    catch (e) {
+        console.error(e)
+        return res.status(500).json({ error: "ERROR: DB query to create/update failed." })
+    }
 })
-
-// app.post("/api/account", async (req, res) => {
-//     const email = req.body.email
-//     const username = req.body.username
-//     const password = req.body.password
-
-//     if (!email || !username || !password) {
-//         return res.status(500).json("USER SERVICE ERROR: Missing account setup fields in request body")
-//     }
-
-//     try {
-//         await pgdb.create_account(username, email, password)
-//         send_email(email, "SFU Collaborative Learning platform: Account Created!", "Thank you for using our platform! Your account was successfully created.")
-//         return res.status(200).json("USER SERVICE: Account successfully created.");
-//     } catch (e) {
-//         console.warn(e);
-//         return res.status(500).json(e);
-//     }
-// })
-
-// app.delete("/api/account", async (req, res) => {
-//     const username = req.body.username;
-//     if (!username) {
-//         return res.status(500).json("USER SERVICE ERROR: Missing username in request body");
-//     }
-
-//     try {
-//         await pgdb.delete_account(username)
-//         return res.status(200).json("USER SERVICE: Account successfully deleted.");
-//     } catch (e) {
-//         console.warn(e);
-//         return res.status(500).json(e);
-//     }
-// })
-
-// app.get("/api/account/email/:email", async (req, res) => {
-//     const email = req.params.email;
-//     if (!email) {
-//         return res.status(500).json("USER SERVICE ERROR: Missing email in request params");
-//     }
-
-//     try {
-//         const account = await pgdb.get_account_by_email(email);
-//         return res.status(200).json(account);
-//     } catch (e) {
-//         console.warn(e);
-//         return res.status(500).json("USER SERVICE ERROR: Databse query failed");
-//     }
-// })
-
-// app.get("/api/accounts", async (req, res) => {
-//     try {
-//         const allAccounts = await pgdb.get_accounts();
-//         return res.status(200).json(allAccounts);
-//     } catch (e) {
-//         console.warn(e);
-//         return res.status(500).json("USER SERVICE ERROR: Databse query failed");
-//     }
-// });
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log("PROFILE SERVICE: running on port 8080");
