5daa2caaf332467f11e407823f3d8e38c8e0cbd7
yenu <35312312+yenuGH@users.noreply.github.com>
2024-04-01 15:50:17 -0700

    allow for cloud run configurtation of firestore

--- services/quiz-service/routes/quiz.js
+++ services/quiz-service/routes/quiz.js
@@ -1,12 +1,44 @@
 /* ----------------------------------- setup ----------------------------------- */
-const dotenv = require("dotenv");
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
+const dotenv = require('dotenv');
+
 dotenv.config();
-const admin = require('firebase-admin');
-const credentials = require(`../${process.env.FIRESTORE_CONFIG}`)
-admin.initializeApp({
-    credential: admin.credential.cert(credentials)
-})
-const db = admin.firestore();
+const firebaseConfig = process.env.FIREBASE_CONFIG;
+const firestoreId = process.env.FIRESTORE_DB_ID;
+
+let firebaseApp;
+let db;
+
+try {
+    if (firebaseConfig != "CLOUD") {
+        const serviceAccount = require(`../${firebaseConfig}`);
+
+        firebaseApp = initializeApp({
+            credential: cert(serviceAccount)
+        });
+        console.log("Connected to Firebase Firestore locally with firebase configuration file");
+    }
+    else {
+        firebaseApp = initializeApp();
+        console.log("Connected to Firebase Firestore over cloud configuration");
+    }
+}
+catch (error) {
+    console.log("\x1b[31m", "ERROR: Unable to connect to Firestore Instance\nAre you running locally or over the cloud?");
+    throw error;
+}
+
+// https://stackoverflow.com/questions/77061688/how-can-i-connect-to-a-non-default-firestore-database-using-node-using-multiple
+// try connecting to the firestore database defined in the environment variables
+try {
+    db = getFirestore(firebaseApp, firestoreId);
+    console.log(`Connected to Firestore Database: ${firestoreId}`);
+}
+catch (error) {
+    console.log("\x1b[31m", "ERROR: Unable to connect to Firestore Database\nIs the Firestore Database ID correct?");
+    throw error;
+}
 
 /* -------------------------------- CRUD handlers ------------------------------- */
 
